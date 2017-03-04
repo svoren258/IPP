a381de7fb5c94ac4478c812d90b22a7b9e73c504
Ondrej Svoreň <svoren258@a04-0827b.kn.vutbr.cz>
2017-03-04 18:31:09 +0100

    regex

--- syn.php
+++ syn.php
@@ -25,10 +25,11 @@
 	}
 
 	foreach (range(1, $argc) as $argnum) {
+
 		if (substr($argv[$argnum], 0, 9) == "--format=") {
-			echo "mam format\n";
 			$format_file = substr($argv[$argnum], 9, strlen($argv[$argnum]));
 			$myformatfile = fopen($format_file, "r");
+
 			if ($myformatfile == FALSE) {
 				fwrite(STDERR, "Format file doesn't exist or error by opening format file for reading.\n");
 				exit(4);
@@ -37,21 +38,21 @@
 	}
 	
 	foreach (range(1, $argc) as $argnum) {
-		echo "som vo foreachi pre input\n";
+
 		if (substr($argv[$argnum], 0, 8) == "--input=") {
-			echo "mam input\n";
 			$input_file = substr($argv[$argnum], 8, strlen($argv[$argnum]));
 			$myinputfile = fopen($input_file, "r");
+
 			if ($myinputfile == FALSE) {
 				fwrite(STDERR, "Error by opening input file for reading.\n");
 				exit(2);
 			}
 			foreach (range(1, $argc) as $argnum) {
-				echo "som vo foreachi pre output\n";
+
 				if (substr($argv[$argnum], 0, 9) == "--output=") {
-					echo "mam output\n";
 					$output_file = substr($argv[$argnum], 9, strlen($argv[$argnum]));
 					$myoutputfile = fopen($output_file, "w");
+
 					if ($myoutputfile == FALSE) {
 						fwrite(STDERR, "Error by opening input file for writing.\n");
 						exit(3);
@@ -61,7 +62,6 @@
 				}
 			}
 			if ($myoutputfile == "") {
-				echo "nemam output\n";
 				$myoutputfile = fopen("php://stdout", "w");
 				inToOut($myinputfile, $myoutputfile, $myformatfile, FALSE);
 				break;
@@ -70,12 +70,15 @@
 	}
 
 	if ($myinputfile == "") {
-		echo "nemam input\n";
+
 		$inputContent = trim(fgets(STDIN));
+
 		foreach (range(1, $argc) as $argnum) {
+
 			if (substr($argv[$argnum], 0, 9) == "--output=") {
 				$output_file = substr($argv[$argnum], 9, strlen($argv[$argnum]));
 				$myoutputfile = fopen($output_file, "w");
+
 				if ($myoutputfile == FALSE) {
 					fwrite(STDERR, "Output file doesn't exist or error by opening input file for writing.\n");
 					exit(3);
@@ -83,7 +86,6 @@
 					
 				inToOut($inputContent, $myoutputfile, $myformatfile, TRUE);
 				break;
-					
 			}
 		}
 		if (($myoutputfile == "")) {
@@ -97,7 +99,7 @@
  		global $argv;
  	
  			$arrayOfChars = str_split($inputContent);
- 			if ($arrayOfChars[strlen($line)-1] != "\n") {
+ 			if ($arrayOfChars[strlen($inputContent)-1] != "\n") {
 					array_push($arrayOfChars, "\n");		
 				}
 			$inputContent = implode($arrayOfChars);
@@ -136,9 +138,10 @@
 		if ($myinputfile != "") {
 			fclose($myinputfile);	
 		} 
-		#pred uzavretim suboru prechadzam na dalsi riadok
+
 		fwrite($myoutputfile, "\n");
 		fclose($myoutputfile);
+
 		if ($myformatfile != "") {
 			fclose($myformatfile);
 		}
@@ -155,18 +158,14 @@
 			global $arrayOfElem;
 			global $arrayOfRules;
 			global $arrayOfPattern;
-			// $output_array = array();
-			
-			#$elements = implode(" ", $arrayOfElem);
+
 			if (empty($arrayOfRules)) {
 				while(!feof($myformatfile)){
 				$arrayOfElem = array();
 				$regexp = "";
-				#echo "som vo while\n";
 				$tab = FALSE;
 				
 				$line = fgets($myformatfile);
-				//echo $line."\n";
 				$arrayOfChars = str_split($line);
 
 				
@@ -175,12 +174,12 @@
 				}
  					
 				foreach ($arrayOfChars as $char) {
-
  					if ($char == "\t") {
+
  						$tab = TRUE;
  						$regexp = $string;
+
  						$string = "";
- 						#echo $regexp."\n";
  						continue;
  					}
  					elseif (($char == ",") || ($char == "\n")) {
@@ -190,38 +189,22 @@
  					}
  					elseif ($char == " "){;
  						if ($tab == FALSE) {
- 							#echo "som v ife v ktorom nemam byt\n";
  							$string .= $char;
  						}
  						continue;
  					}
  					$string .= $char;
- 					#echo $string."\n";
  				}
-			
- 			$pattern = "/".$regexp."/";
- 			$arrayOfRules[$pattern] = $arrayOfElem;
- 			//$arrayOfRules["/je/"] = $array = array("bold");
- 			//print_r($arrayOfRules);
- 			
-		}
 
-		// foreach ($arrayOfRules as $rule) {
- 	// 				foreach ($rule as $elements) {
- 	// 					echo $elements."\n";
- 	// 				}
- 	// 			}
+			
+ 			regexpSettings($regexp);
+ 			$pattern = "/".$regexp."/";
+ 			$arrayOfRules[$pattern] = $arrayOfElem;	
+		}
 		
 		addTag($arrayOfRules, $inputContent);
 			}
 			else {
-				// foreach ($arrayOfRules as $rule) {
- 			// 		foreach ($rule as $elements) {
- 			// 			echo $elements."\n";
- 			// 		}
- 			// 	}
- 				//exit;
-				//$pattern = "/".$regexp."/";
 				addTag($arrayOfRules, $inputContent);
 			}
 			
@@ -234,6 +217,8 @@
 
 	function addTag($arrayOfRules, &$inputContent) {
 		$output_array = array();
+		$approvedChars = array();
+		$repeat = FALSE;
 		$count = 0;
 		
  				foreach ($arrayOfRules as $rule) {
@@ -244,70 +229,91 @@
 		 				//echo $elem."\n";
 		 				
 		 				 switch($elem) {
+
 		 					case "bold":
-		 						//echo $inputContent."\n";
-		 						//echo $pattern."\n";
-		 						preg_match($pattern, $inputContent, $output_array);
-		 						foreach($output_array as $item) {
-		 							//echo $item."\n";
-		 							//echo $inputContent."\n";
-		 							$inputContent = preg_replace($pattern, "<b>".$item."</b>", $inputContent);
-		 							#echo $inputContent."\n";
-		 						}
+
+								preg_match_all($pattern, $inputContent, $output_array);
+			 						
+			 						foreach($output_array as $array) {
+			 							removeDuplicate($array);
+			 							foreach ($array as $item) {
+			 								$inputContent = preg_replace("/".$item."/", "<b>".$item."</b>", $inputContent);
+			 							}
+			 						}
 		 						break;
 
 		 					case "italic":
-		 						//echo $inputContent."\n";
-		 						//echo $pattern."\n";
 
-		 						preg_match($pattern, $inputContent, $output_array);
-		 						
-		 						foreach($output_array as $item) {
-		 							//echo $item."\n";
-		 							//echo $inputContent."\n";
-		 							$inputContent = preg_replace($pattern, "<i>".$item."</i>", $inputContent);
-		 							#echo $inputContent."\n";
-		 						}
+		 						preg_match_all($pattern, $inputContent, $output_array);
+								
+			 						foreach($output_array as $array) {
+			 							removeDuplicate($array);
+			 							foreach ($array as $item) {
+		 									$inputContent = preg_replace("/".$item."/", "<i>".$item."</i>", $inputContent);
+			 							}
+			 						}
 		 						break;
 
 		 					case "underline":
-		 						preg_match($pattern, $inputContent, $output_array);
-		 						foreach($output_array as $item) {
-		 							$inputContent = preg_replace($pattern, "<u>".$item."</u>", $inputContent);
-		 						}
+		 						preg_match_all($pattern, $inputContent, $output_array);
+								
+			 						foreach($output_array as $array) {
+			 							removeDuplicate($array);
+			 							foreach ($array as $item) {
+		 									$inputContent = preg_replace("/".$item."/", "<u>".$item."</u>", $inputContent);
+			 							}
+			 						}
 		 						break;
 
 		 					case "teletype":
-		 						preg_match($pattern, $inputContent, $output_array);
-		 						foreach($output_array as $item) {
-		 							$inputContent = preg_replace($pattern, "<tt>".$item."</tt>", $inputContent);
-		 						}
+		 						preg_match_all($pattern, $inputContent, $output_array);
+								//print_r($output_array);
+								// print_r($output_array);
+			 						foreach($output_array as $array) {
+			 							removeDuplicate($array);
+			 							foreach ($array as $item) {
+			 								#echo $item."\n";
+		 									$inputContent = preg_replace("/".$item."/", "<tt>".$item."</tt>", $inputContent);
+			 								#echo $inputContent."\n";
+			 							}
+			 						}
 		 						break;
 						}
+						//$approvedChars = [];
 		 				if (substr($elem, 0, 5) == "size:") {
 		 					$size = substr($elem, 5, strlen($elem));
-							preg_match($pattern, $inputContent, $output_array);
-		 					foreach($output_array as $item) {
-		 						$inputContent = preg_replace($pattern, "<font size=".$size.">".$item."</font>", $inputContent);
-		 					}
+		 			
+							preg_match_all($pattern, $inputContent, $output_array);
+								
+			 						foreach($output_array as $array) {
+			 							removeDuplicate($array);
+			 							foreach ($array as $item) {
+		 									$inputContent = preg_replace("/".$item."/", "<font size=".$size.">".$item."</font>", $inputContent);
+			 							}
+			 						}
 						}
+
 		 				elseif (substr($elem, 0, 6) == "color:") {
+		 					
 		 					$color = substr($elem, 6, strlen($elem));
-		 					preg_match($pattern, $inputContent, $output_array);
-		 					foreach($output_array as $item) {
-		 						$inputContent = preg_replace($pattern, "<font color=#".$color.">".$item."</font>", $inputContent);
-		 					}
-		 				}
-		 			}	 		
-		 	}
-	}
+		 					preg_match_all($pattern, $inputContent, $output_array);
+								
+			 						foreach($output_array as $array) {
+			 							removeDuplicate($array);
+			 							foreach ($array as $item) {
+		 									$inputContent = preg_replace("/".$item."/", "<font color=#".$color.">".$item."</font>", $inputContent);
+			 							}
+			 						}
+		 				}	 		
+		 			}
+				}
+			}
+
 
 	function newPattern($arrayOfRules, $count) {
 		$num = 0;
 		while($array = current($arrayOfRules)){
- 			//echo key($arrayOfRules)."\n";
  			$pattern = key($arrayOfRules);
- 			//echo $pattern."\n";
  			
  			if ($num == $count) {
  				return $pattern;
@@ -316,4 +322,58 @@
  			$num++;
  		}
 	}
+
+	function regexpSettings(&$regexp) {
+		switch ($regexp) {
+			case "%s":
+				$regexp = "\s";
+				break;
+			
+			case "%a":
+				$regexp = "\S";
+				break;
+
+			case "%d":
+				$regexp = "[0-9]";
+				break;
+
+			case "%l":
+				$regexp = "[a-z]";
+				break;
+
+			case "%L":
+				$regexp = "[A-Z]";
+				break;
+
+			case "%w":
+				$regexp = "[a-zA-Z]";
+				break;
+
+			case "%W":
+				$regexp = "[a-zA-Z0-9]";
+				break;
+
+			case "%t":
+				$regexp = "\t";
+				break;
+
+			case "%n":
+				$regexp = "\n";
+				break;
+
+			default:
+				break;
+		}
+	}
+
+function removeDuplicate(&$array) {
+	$count = count($array);
+	for ($i=0; $i < $count; $i++) {
+		for ($j=$i+1; $j < $count; $j++){
+			if ($array[$i] == $array[$j]) {
+				unset($array[$i]);
+			}
+		}
+	}		
+}
 ?>
